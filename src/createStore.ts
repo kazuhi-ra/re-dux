b9a90327d2f73fe6d382cce07aa27fcd717a6996
kazuhi-ra <kazuhira4126@gmail.com>
2021-08-09 22:42:24 +0900

    subscribe機能を追加する

--- src/createStore.ts
+++ src/createStore.ts
@@ -7,6 +7,9 @@ class Store {
   currentReducer: Reducer
   currentState: State
   isDispatching = false
+  currentListeners: (() => void)[] = []
+  nextListeners = this.currentListeners
+  isSubscribed = false
 
   constructor(args: { reducer: Reducer; initialState: State }) {
     const { reducer, initialState } = args
@@ -32,7 +35,20 @@ class Store {
       this.isDispatching = false
     }
 
+    this.execNextListeners()
+
     return action
+  }
+
+  subscribe = (listener: () => void) => {
+    if (this.isDispatching) {
+      throw new Error('reducerの実行中です')
+    }
+    this.isSubscribed = true
+    this.ensureCanMutateNextListeners()
+    this.nextListeners.push(listener)
+
+    return this.unsubscribe
   }
 
   private init = (): void => {
@@ -46,11 +62,42 @@ class Store {
   }
 
   private actionValidator = (action: Action): void => {
-    if (!!action.type) {
+    if (!action.type) {
       throw new Error(
         'actionはtypeプロパティを持つオブジェクトでなければいけません'
       )
     }
+  }
+
+  // dispatch中にsubscribeを呼ぶのを防ぐためのメソッド
+  private ensureCanMutateNextListeners = (): void => {
+    if (this.nextListeners === this.currentListeners) {
+      this.nextListeners = this.currentListeners.slice()
+    }
+  }
+
+  private execNextListeners = () => {
+    const listeners = (this.currentListeners = this.nextListeners)
+    if (!listeners) return
+
+    for (let i = 0; i < listeners.length; i++) {
+      const listener = listeners[i]
+      listener()
+    }
+  }
+
+  private unsubscribe = (listener: () => void) => {
+    if (!this.isSubscribed) return // isSubscribedがfalseの時に呼び出されても何もしない
+    if (this.isDispatching) {
+      throw new Error('reducerの実行中です')
+    }
+
+    this.isSubscribed = false
+
+    this.ensureCanMutateNextListeners()
+    const index = this.nextListeners.indexOf(listener)
+    this.nextListeners.splice(index, 1)
+    this.currentListeners = []
   }
 }
 
